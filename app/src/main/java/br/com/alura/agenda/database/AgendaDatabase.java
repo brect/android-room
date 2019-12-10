a117842c165146be56b6d206a8f10e85c394ecae
brect <b.limas93@gmail.com>
2019-12-09 21:03:37 -0300

    O que são migrations, e como funcionam no Room;
    Destruir as migrations, para mudar o banco de dados quando temos mudança na entidade;
    
    Usar a destruição de migration apenas durante o desenvolvimento e enquanto o App não for publicado;
    
    Implementar migration para adicionar campos novos.

--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -1,15 +1,18 @@
 package br.com.alura.agenda.database;
 
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import br.com.alura.agenda.database.dao.AlunoDAO;
 import br.com.alura.agenda.model.Aluno;
 
-@Database(entities = {Aluno.class}, version = 2, exportSchema = false)
+@Database(entities = {Aluno.class}, version = 3, exportSchema = false)
 public abstract class AgendaDatabase extends RoomDatabase {
 
     private static final String NOME_BANCO_DE_DADOS = "agenda.db";
@@ -20,6 +23,23 @@ public abstract class AgendaDatabase extends RoomDatabase {
         return Room
                 .databaseBuilder(context, AgendaDatabase.class, NOME_BANCO_DE_DADOS)
                 .allowMainThreadQueries()
+                .addMigrations(new Migration(1, 2) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase database) {
+                        database.execSQL("ALTER TABLE aluno ADD COLUMN sobrenome TEXT");
+                    }
+                }, new Migration(2, 3) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase database) {
+                        //Criar nova tabela com as informacoes desejadas
+
+                        //Ciouar dados da tabela antiga para a nova
+
+                        //Remove tabela antiga
+
+                        //Renomear a tabela nova com o nome da tabela antiga
+                    }
+                })
                 .build();
     }
 }
